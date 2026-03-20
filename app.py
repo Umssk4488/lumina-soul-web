2a46dd846cd279c6f9258b4dca7d0a53e3c0ff87
Umssk4488 <umumssk@gmail.com>
2026-03-20 14:53:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_icon="🔮",
     layout="centered"
 )
-st.image("banner.jpg.png", use_container_width=True)
+st.image("banner.jpg.png", use_container_width=500)
 
 # -----------------------------
 # Language state
